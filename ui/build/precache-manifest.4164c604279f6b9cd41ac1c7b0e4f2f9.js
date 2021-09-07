d364981b55005d3159500fb980f96db43328dc59
Mario HD <mariohdpz@gmail.com>
2021-09-07 19:18:55 +0000

    Rebuild app with new logo

---
+++ ui/build/precache-manifest.4164c604279f6b9cd41ac1c7b0e4f2f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef159a960f3cebbd7ff05ab70f50995c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1460676b31bf4c7da82f",
+    "url": "/static/css/2.c38cd9da.chunk.css"
+  },
+  {
+    "revision": "1460676b31bf4c7da82f",
+    "url": "/static/js/2.36a41000.chunk.js"
+  },
+  {
+    "revision": "28ce00ad444b69bea42850e06eec9da3",
+    "url": "/static/js/2.36a41000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf55739a64005f5e483",
+    "url": "/static/js/main.0deb9e3a.chunk.js"
+  },
+  {
+    "revision": "0e903404fe668039b1a7",
+    "url": "/static/js/runtime-main.2a78626f.js"
+  },
+  {
+    "revision": "8c704ca59f54e8454c24b50dcaa12c40",
+    "url": "/static/media/logo.8c704ca5.svg"
+  }
+]);
